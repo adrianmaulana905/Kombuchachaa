21c97cd6065513dbb87a48596c994652ecd93546
adrianmaulana905 <v3in10game@gmail.com>
2026-05-13 09:30:26 +0700

    Add files via upload

---
+++ Kombuchacha.html/kombuchacha.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
